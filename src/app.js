9bdf3dbce6f072ec9c2895110594f9c96eb9570e
adchavesm <adchavesm@unal.edu.co>
2019-11-11 01:49:17 -0500

    Final

--- src/app.js
+++ src/app.js
@@ -1,67 +1,76 @@
 const express= require('express');
 const bodyParser=require('body-parser');
 const passport=require('passport');
+const cors = require('cors');
 const ldapstrategy=require('passport-ldapauth');
 const ldap = require('ldapjs');
 const morgan= require('morgan');
+const router = express.Router();
+
+const app = express()
+
 var client = ldap.createClient({
     url: 'ldap://localhost:389'
 });
-client.bind('cn=admin,dc=arqsoft,dc=unal,dc=edu,dc=co','admin',()=>{
+
+const run = async() => 
+    await client.bind('cn=admin,dc=arqsoft,dc=unal,dc=edu,dc=co','admin',()=>{
     console.log("Conectado a ldap");
 });
 
-const OPTS = {
-    server: {
-      url: "ldap://localhost:389",
-      bindDN: "cn=read-only-admin,dc=example,dc=com",
-      bindCredentials: 'password',
-      searchBase: "dc=example,dc=com",
-      searchFilter: "(uid={{username}})"
-    }
-  }
-  
-/*
-var entry={
-    cn:'pedro sanchez',
-    sn:'Sanchez',
-    givenName:'Pedro',
-    objectclass:'inetOrgPerson',
-    userPassword:'asudh2ud2asksow',
-    gidNumber:500,
-    UserName:'pdsanchez@unal.edu.co',
-};
 
-const run=async()=>{
-   await client.add('cn=jpaez@unal.edu.co,ou=academy,dc=arqsoft,dc=unal,dc=edu,dc=co',entry,()=>{
-        console.log("sdsassssssssssssd")
-   });
-}
-run();
-*/
-
-//PRUEBA PARA AUTENTICAR, NO SIRVIO
-
-
-passport.use(new ldapstrategy(OPTS))
-
-// instantiate the server
-const app = express()
 app.use(morgan('dev'));
-// parse the request data automatically
 app.use(bodyParser.json())
-// allow cross origin resource sharing
-// inject LDAP connection to express server
-app.use(passport.initialize())
-
+app.use(passport.initialize());
+app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
-
-// listen to port defined
 const port = process.env.PORT || 8086;
-app.listen(port, () => {
+
+app.listen(port, async() => {
   console.log(`Listening on port ${port}`)
+  await run();
+});
+run();
+
+
+
+app.delete('/delete',async(req,res)=>{
+  await run();
+  await client.del(`cn=${req.body.user},ou=academy,dc=arqsoft,dc=unal,dc=edu,dc=co`,(err)=>{
+    if(err){
+      res.send({
+        answer:"Unsuccessfully"
+      });
+    }else{
+      res.send('successfully');
+    }
+  })
 });
 
-app.post('/login', passport.authenticate('ldapauth', {session: false}), function(req, res) {
-    res.send({status: 'ok'});
-  });
+app.post('/add',async(req,res)=>{
+  await run();
+  await client.add(`cn=${req.body.cn},ou=academy,dc=arqsoft,dc=unal,dc=edu,dc=co`,req.body, (err)=>{
+       if(err){
+        res.send({
+          answer:"Unsuccessfully"
+        });
+       }else{
+        res.send({
+          answer:"Successfully"
+        });
+       }
+  })
+});
+
+  app.post('/log', (req,res) =>{
+    client.bind(`cn=${req.body.username},ou=academy,dc=arqsoft,dc=unal,dc=edu,dc=co`,req.body.password, (err)=>{
+      if(err){
+        res.send({
+          answer:"Unsuccessfully"
+        });
+      }else
+      res.send({
+        answer:"Successfully"
+      });
+    })
+  })
